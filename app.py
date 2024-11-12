df75bc91d3669fe9aa15ef1e04e4878fcf4cb53c
Jun Hu <hu.jun@iisgalvanimi.edu.it>
2024-11-12 11:48:26 +0000

    terzo commit

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask,  render_template, request, jsonify
 app = Flask(__name__)
 #server web, codice per istituire l'homepage
-
 
 @app.route('/')
 # / = homepage, pagina di default
 def homepage():
     return render_template('homepage.html')
 
-
-
 #server API 
 @app.route('/calcola', methods=['POST'])
 def calcola():
-    dati = request.get_json()
 
+    #prende le informazioni in arrivo dal front-end
+    json_data = request.get_json()
+    if json_data:
+        num1 = json_data.get('num1')
+        num2 = json_data.get('num2')
+        operazione = json_data.get('operazione')
+
+        #elaborazione delle informazioni
+        if operazione == 'addizione':
+            risultato = num1 + num2
+        elif operazione == 'sottrazione':
+            risultato = num1 - num2
+        elif operazione == 'moltiplicazione':
+            risultato = num1 * num2
+        elif operazione == 'divisione':
+            risultato = num1 // num2
+        #restituzione dei risutati al front end
+        return jsonify(risultato = risultato)
+    else:
+        return jsonify(risultato = 'mancano i dati')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
---
+++ app1.py
@@ -0,0 +1,1 @@
+#esercizio: realizzare una SPA che effetui le 4 operazioni fondamentali utilizzando il metodo GET 
